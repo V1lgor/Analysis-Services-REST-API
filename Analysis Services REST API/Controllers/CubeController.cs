940324ccf705041f1764369c76b8d7291a6afdd3
Игорь <igor.volkov1999@gmail.com>
2020-05-23 11:02:17 +0400

    Created API method for getting cube name list

---
+++ Analysis Services REST API/Controllers/CubeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AnalysisServices.AdomdClient;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Analysis_Services_REST_API.Models;
+
+namespace Analysis_Services_REST_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CubeController : ControllerBase
+    {
+        public string Get()
+        {
+            using(AdomdConnection mdConn = new AdomdConnection())
+            {
+                mdConn.ConnectionString = "provider=msolap;Data Source=V1LGORPC\\ASMAIN;initial catalog=AdventureWorksDW2014Multidimensional-EE;";
+                mdConn.Open();
+
+                List<Cube> cubeNameList = new List<Cube>();
+
+                foreach(CubeDef cube in mdConn.Cubes)
+                {
+                    if (cube.Name.StartsWith('$')) continue;
+
+                    cubeNameList.Add(new Cube { Name = cube.Name });
+                }
+
+                return JsonConvert.SerializeObject(cubeNameList, Formatting.Indented);
+            }
+        }
+    }
+}
---
+++ Analysis Services REST API/Models/Cube.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Analysis_Services_REST_API.Models
+{
+    public class Cube
+    {
+        public String Name { get; set; }
+    }
+}
